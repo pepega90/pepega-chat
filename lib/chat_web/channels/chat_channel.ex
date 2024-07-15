8e81e8dcb49c25e2f4490c1069c2d761e9a6c18c
pepega90 <asuarif5@gmail.com>
2024-07-15 22:00:39 +0700

    feat: handle when user disconnect and some refactoring

--- lib/chat_web/channels/chat_channel.ex
+++ lib/chat_web/channels/chat_channel.ex
@@ -7,17 +7,19 @@ defmodule ChatWeb.ChatChannel do
   def join("chat:lobby", payload, socket) do
     if authorized?(payload) do
       send(self(), "after_join")
-      {:ok, socket}
+      {:ok, socket |> assign(users: %{})}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
+  @impl true
   def handle_info("after_join", socket) do
     Presence.track(self(), "after_join", socket.id, %{})
 
-    [data] = Presence.list("after_join")
-    |> Enum.map(fn {_, data} -> data[:metas] end)
+    [data] =
+      Presence.list("after_join")
+      |> Enum.map(fn {_, data} -> data[:metas] end)
 
     push(socket, "orang", %{data: data})
 
@@ -31,17 +33,32 @@ defmodule ChatWeb.ChatChannel do
     {:reply, {:ok, payload}, socket}
   end
 
-  def handle_in("chat:newUser", %{"user" => newUser}, socket) do
+  def handle_in("chat:newUser", %{"user" => newUser}, %{assigns: %{users: users}} = socket) do
     Presence.update(self(), "after_join", socket.id, %{
-      user: newUser,
+      id: inspect(socket.channel_pid),
+      user: newUser
     })
+
+    updated_users = Map.put(users, inspect(socket.channel_pid), newUser) |> IO.inspect()
+
+    %{"" => %{metas: list_user}} = Presence.list("after_join")
+    list_user |> IO.inspect()
+
     broadcast(socket, "chat:newUser", %{newUser: newUser})
-    {:noreply, socket}
+    broadcast(socket, "chat:userJoined", %{user: newUser})
+    {:noreply, socket |> assign(users: updated_users)}
   end
 
   def handle_in("chat:msg", payload, socket) do
     broadcast(socket, "chat:msg", payload)
     {:noreply, socket}
+  end
+
+  @impl true
+  def terminate(_reason, %{assigns: %{users: users}} = socket) do
+    user_name = Map.get(users, inspect(socket.channel_pid))
+    broadcast(socket, "chat:userLeft", %{user: user_name})
+    :ok
   end
 
   # Add authorization logic here as required.
